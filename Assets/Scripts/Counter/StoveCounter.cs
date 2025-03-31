c36b7531485dae127c9de800d888c5bbc4bda7a7
tanggaopeng <tangaopeng@outlook.com>
2025-03-31 23:54:23 +0800

    1.添加煎锅柜台功能

--- Assets/Scripts/Counter/StoveCounter.cs
+++ Assets/Scripts/Counter/StoveCounter.cs
@@ -4,13 +4,106 @@ using UnityEngine;
 
 public class StoveCounter : BaseCounter
 {
+    [SerializeField] private FryingRecipeListSO fryingRecipeList;
+
+    public enum StoveState
+    {
+        Idle,
+        Frying,
+        Burning
+    }
+
+    private FryingRecipe fryingRecipe;
+    private float fryingTimer = 0;
+    private StoveState state = StoveState.Idle;
+
     public override void Interact(Player player)
     {
-        base.Interact(player);
+        if (player.IsHaveKitchenObject())
+        {//手上有食材
+            if (IsHaveKitchenObject() == false && fryingRecipeList.TryGetFryingRecipe(player.GetKichenObject().GetKitchenObjectSO(), 
+                out FryingRecipe fryingRecipe))
+            {//当前柜台为空
+                TransferKitchenObject(player, this);
+                StartFrying(fryingRecipe);
+            }
+            else
+            {
+                //当前柜台不为空
+
+            }
+        }
+        else
+        {
+            // 手上没有食材
+            if (IsHaveKitchenObject() == false)
+            {
+                //当前柜台为空
+
+            }
+            else
+            {
+                //当前柜台不为空
+                TransferKitchenObject(this, player);
+            }
+        }
     }
 
     public override void InteractOperate(Player player)
     {
         base.InteractOperate(player);
+    }
+
+    private void Update()
+    {
+        switch (state)
+        {
+            case StoveState.Idle:
+                break;
+            case StoveState.Frying:
+                fryingTimer += Time.deltaTime;
+                if(fryingTimer >= fryingRecipe.fryingTime)
+                {
+                    DestroyKitchenObject();
+                    CreateKitchenObject(fryingRecipe.output.prefab);
+
+                    fryingRecipeList.TryGetFryingRecipe(GetKichenObject().GetKitchenObjectSO(),
+                        out FryingRecipe newFryingRecipe);
+                    StartBurning(newFryingRecipe);
+                }
+                break;
+            case StoveState.Burning:
+                fryingTimer += Time.deltaTime;
+                if (fryingTimer >= fryingRecipe.fryingTime)
+                {
+                    DestroyKitchenObject();
+                    CreateKitchenObject(fryingRecipe.output.prefab);
+                    state = StoveState.Idle;
+                }
+                break;
+            default: 
+                break;
+        }
+    }
+
+    private void StartFrying(FryingRecipe fryingRecipe)
+    {
+        fryingTimer = 0;
+        this.fryingRecipe = fryingRecipe;
+        state = StoveState.Frying;
+    }
+
+    private void StartBurning(FryingRecipe fryingRecipe)
+    {
+        if(fryingRecipe == null)
+        {
+            Debug.LogWarning("无法获取Burning的食谱，无法进行Burning");
+            state = StoveState.Idle;
+            return;
+        }
+
+        fryingTimer = 0;
+        this.fryingRecipe = fryingRecipe;
+        state = StoveState.Burning;
     }
 }
