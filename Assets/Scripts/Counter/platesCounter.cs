3a6f82326c866b90fe26903ffed91f1506e105dd
tanggaopeng <tangaopeng@outlook.com>
2025-04-05 15:54:21 +0800

    1.添加盘子柜台

---
+++ Assets/Scripts/Counter/platesCounter.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class platesCounter : BaseCounter
+{
+    public override void Interact(Player player)
+    {
+        base.Interact(player);
+    }
+}
