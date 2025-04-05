1758511e737bc9b5f7ee5d394df53c28c8725fb8
tanggaopeng <tangaopeng@outlook.com>
2025-04-05 18:10:24 +0800

    1.添加自动生成盘子功能

--- Assets/Scripts/Counter/platesCounter.cs
+++ Assets/Scripts/Counter/platesCounter.cs
@@ -4,8 +4,43 @@ using UnityEngine;
 
 public class platesCounter : BaseCounter
 {
+    [SerializeField] private KitchenObjectSO plateSO;
+    [SerializeField] private float spawnRate = 3;
+    [SerializeField] private int plateCounterMax = 5;
+
+    private List<KitchenObject> platesList = new List<KitchenObject>();
+
+    private float timer = 0;
+
+    private void Update()
+    {
+        if(platesList.Count < plateCounterMax)
+        {
+            timer += Time.deltaTime;
+        }
+        if (timer > spawnRate)
+        {
+            timer = 0;
+            SpawnPlate();
+        }
+    }
+
     public override void Interact(Player player)
     {
         base.Interact(player);
+    }
+
+    public void SpawnPlate()
+    {
+        if (platesList.Count >= plateCounterMax)
+        {
+            timer = 0;
+            return;
+        }
+        KitchenObject kitchenObject = GameObject.Instantiate(plateSO.prefab, GetHoldPoint()).GetComponent<KitchenObject>();
+
+        kitchenObject.transform.localPosition = Vector3.zero + Vector3.up * 0.1f * platesList.Count;
+
+        platesList.Add(kitchenObject);
     }
 }
